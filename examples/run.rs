2335db1e29741a49cd487c1e5f3c28f05a834e4b
pearse <pearsedoolin@gmail.com>
2026-05-17 14:48:24 +0200

    Fix publish

--- examples/run.rs
+++ examples/run.rs
@@ -5,7 +5,11 @@ use sonnetics_core::WakeEngine;
 use std::path::Path;
 
 fn main() -> Result<()> {
-    let mut engine = WakeEngine::from_path(Path::new("path/to/model"), 16_000, 1)?;
+    // Download a "Hey Alfred" model from the Sonnetics CDN:
+    // https://cdn.sonnetics.com/models/sonnetics-model-efea8354-3f81-4c61-9d50-7452cb901620.tar.gz
+    // Or download your own custom model from the sonnetics dashboard
+
+    let mut engine = WakeEngine::from_path(Path::new("path/to/extracted/model"), 16_000, 1)?;
     let audio_samples = vec![0.0_f32; 8_000];
     let phrase = engine.detect(&audio_samples, 0.25)?;
     println!("{:?}", phrase);
